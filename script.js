f02f3981bac369af7eb61e5f77abfb7e1fc077bc
Claire <70619108+clairetyson89@users.noreply.github.com>
2022-02-02 21:54:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-var map = L.map('map').setView([38.62936606062699, -90.25318557158936], 12);
+var map = L.map('map').setView([38.639005110722664, -90.2845213858883], 12);
 
-  // load a tile layer
- L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
-	minZoom: 0,
+	minZoom: 1,
 	maxZoom: 20,
 	ext: 'png'
 }).addTo(map);
 
-$.getJSON("points.geojson",function(data){
-    L.geoJson(data).addTo(map);
-  });
+ $.getJSON("https://https://raw.githubusercontent.com/clairetyson89/Assignment1_Restaurants/main/points.geojson",function(data){
+    L.geoJSON(data, {
+  	onEachFeature: function (feature, layer) {
+    		layer.bindPopup('<h3>'+feature.properties.Restaurant+'</h3>');
+  		}
+	}).addTo(map);
+ });
