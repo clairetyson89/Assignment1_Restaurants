486bc7fedeb135e29568aa59170ed3f6df25e640
Claire <70619108+clairetyson89@users.noreply.github.com>
2022-02-02 22:30:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([38.639005110722664, -90.2845213858883], 12);
+var map = L.map('map').setView([38.639005110722664, -90.2845213858883], 11.5);
 
 L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
